2bb0d5fd7bd57f423fdbe3d00735849db018a6c4
yuanzhenXu <479359453@qq.com>
2017-03-06 22:59:51 +0800

    添加相关页面

---
+++ app/view/view_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.view module', function() {
+
+    beforeEach(module('myApp.view'));
+
+    describe('view controller', function(){
+
+        it('should ....', inject(function($controller) {
+            //spec body
+            var viewCtrl = $controller('ViewCtrl');
+            expect(viewCtrl).toBeDefined();
+        }));
+
+    });
+});
---
+++ app/view5/view5_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.view5 module', function() {
+
+    beforeEach(module('myApp.view5'));
+
+    describe('view5 controller', function(){
+
+        it('should ....', inject(function($controller) {
+            //spec body
+            var view5Ctrl = $controller('View5Ctrl');
+            expect(view5Ctrl).toBeDefined();
+        }));
+
+    });
+});
